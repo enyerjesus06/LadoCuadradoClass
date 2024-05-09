dd7c1a043648d23dc112d5e5079b8c982b1c1052
Enyer Jesús Espino Medina <20240199@itla.edu.do>
2024-05-09 15:14:57 -0400

    Aqui esta!!

---
+++ Calculo de los lados.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+public class Class1
+{
+    public void Calladocuadrado()
+    
+    {
+
+        //Declarar las Variables: lado, perimetro, constante...
+
+        int lado = 0;
+        int perimetro = 0;
+        int constante = 4;
+        string linea = string.Empty;
+
+        Console.WriteLine("Ingrese la longitud del lado del cuadrado: ");
+            linea = Console.ReadLine();
+            lado = Convert.ToInt32(linea);
+
+        perimetro = (lado * constante);
+
+        Console.WriteLine($"El perímetro del cuadrado es: { perimetro } ");
+    
+    
+    }
+
+
+
+}
+
